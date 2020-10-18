60246370b6e1731c7f951cbcf8760964544f3135
lucap <luc@rissc.de>
2020-10-18 14:25:21 +0200

    Added throttle

---
+++ src/Jobs/Middleware/Throttle.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Luca Perna - Webdeveloper
+ * Team Dementia
+ * luc@rissc.com
+ *
+ * Date: 18.10.20
+ */
+
+namespace MrMadClown\LaravelUtilities\Jobs\Middleware;
+
+use Illuminate\Support\Facades\Redis;
+use MrMadClown\LaravelUtilities\Jobs\ProvidesThrottleKey;
+
+class RateLimited
+{
+    private ?string $throttleKey;
+    private int $delay;
+    private int $limit;
+
+    public function __construct(int $limit = 1, string $throttleKey = null, int $delay = 10)
+    {
+        $this->limit = $limit;
+        $this->throttleKey = $throttleKey;
+        $this->delay = $delay;
+    }
+
+    public function handle($job, $next): void
+    {
+        Redis::throttle($this->getThrottleKey($job))
+            ->block(0)
+            ->allow($this->limit)
+            ->every($this->delay)
+            ->then(
+                fn() => $next($job),
+                fn() => $job->release($this->delay)
+            );
+    }
+
+    private function getThrottleKey($job): string
+    {
+        if ($job instanceof ProvidesThrottleKey) {
+            return $job->getThrottleKey();
+        }
+
+        return $this->throttleKey ?? class_basename($job);
+    }
+}
---
+++ src/Jobs/ProvidesFunnelKey.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Luca Perna - Webdeveloper
+ * Team Dementia
+ * luc@rissc.com
+ *
+ * Date: 18.10.20
+ */
+
+namespace MrMadClown\LaravelUtilities\Jobs;
+
+
+class ProvidesFunnelKey
+{
+
+}
